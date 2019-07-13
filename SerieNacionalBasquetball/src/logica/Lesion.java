2a0c3b54f5c191bde6048a37b8c97db3ba27ceca
Kalina <Kalina@LAPTOP-MLO1PO9G>
2019-07-13 17:59:50 -0400

    Lesion

---
+++ SerieNacionalBasquetball/src/logica/Lesion.java
@@ -0,0 +1,54 @@
+package logica;
+
+import java.util.Date;
+
+public class Lesion {
+
+	private String codigo;
+	private String tipo;
+	private boolean estado;
+	private Date FechaLesion;
+	private int RecoveryTime;
+	public Lesion(String codigo, String tipo, boolean estado, Date fechaLesion, int recoveryTime) {
+		super();
+		this.codigo = codigo;
+		this.tipo = tipo;
+		this.estado = estado;
+		FechaLesion = fechaLesion;
+		RecoveryTime = recoveryTime;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public Date getFechaLesion() {
+		return FechaLesion;
+	}
+	public void setFechaLesion(Date fechaLesion) {
+		FechaLesion = fechaLesion;
+	}
+	public int getRecoveryTime() {
+		return RecoveryTime;
+	}
+	public void setRecoveryTime(int recoveryTime) {
+		RecoveryTime = recoveryTime;
+	}
+	
+	public void recuperacion () {
+	}
+	
+}
