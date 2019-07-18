1ad9c0f6677aaa22a30a4da592841077afd7cf89
pc <pc@DELL-L>
2019-07-18 19:35:40 -0400

    Clase jugador parametros añadidos

--- SerieNacionalBasquetball/src/logica/Jugador.java
+++ SerieNacionalBasquetball/src/logica/Jugador.java
@@ -1,65 +1,154 @@
 package logica;
 
+import java.util.ArrayList;
+
 public class Jugador {
 	private String codigo;
 	private String nombre;
 	private String desempeño;
 	private int numeroCamiseta;
-	private Lesion Lesiones;
+	private ArrayList<Lesion> Lesiones;
+	private int cantlesiones;
 	private Equipo MiEquipo;
 	private int PtsTotalCarrera;
-	
-	public Jugador(String codigo, String nombre, String desempeño, int numeroCamiseta, Lesion lesiones, Equipo miEquipo, int ptsTotalCarrera) {
+	private int cantasistencias;
+	private int cantbloqueo;
+	private int cantrobodebalon;
+	private float altura;
+	private int edad;
+	private float peso;
+
+	public Jugador(String codigo, String nombre, String desempeño, int numeroCamiseta,int cantlesiones, Equipo miEquipo,
+			int ptsTotalCarrera, int cantasistencias, int cantbloqueo, int cantrobodebalon,float altura,int edad, float peso) {
 		super();
 		this.codigo = codigo;
 		this.nombre = nombre;
 		this.desempeño = desempeño;
 		this.numeroCamiseta = numeroCamiseta;
-		Lesiones = lesiones;
+		Lesiones = new ArrayList<Lesion>();
+		this.cantlesiones=cantlesiones;
 		MiEquipo = miEquipo;
 		PtsTotalCarrera = ptsTotalCarrera;
+		this.cantasistencias = cantasistencias;
+		this.cantbloqueo = cantbloqueo;
+		this.cantrobodebalon = cantrobodebalon;
+		this.altura = altura;
+		this.edad=edad;
+		this.peso=peso;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
-	public Lesion getLesiones() {
+
+	public ArrayList<Lesion> getLesiones() {
 		return Lesiones;
 	}
-	public void setLesiones(Lesion lesiones) {
+
+	public void setLesiones(ArrayList<Lesion> lesiones) {
 		Lesiones = lesiones;
 	}
+	
+	public int getCantlesiones() {
+		return cantlesiones;
+	}
+
+	public void setCantlesiones(int cantlesiones) {
+		this.cantlesiones = cantlesiones;
+	}
+
 	public Equipo getMiEquipo() {
 		return MiEquipo;
 	}
+
 	public void setMiEquipo(Equipo miEquipo) {
 		MiEquipo = miEquipo;
 	}
+
 	public int getPtsTotalCarrera() {
 		return PtsTotalCarrera;
 	}
+
 	public void setPtsTotalCarrera(int ptsTotalCarrera) {
 		PtsTotalCarrera = ptsTotalCarrera;
 	}
+
 	public String getDesempeño() {
 		return desempeño;
 	}
+
 	public void setDesempeño(String desempeño) {
 		this.desempeño = desempeño;
 	}
+
 	public int getNumeroCamiseta() {
 		return numeroCamiseta;
 	}
+
 	public void setNumeroCamiseta(int numeroCamiseta) {
 		this.numeroCamiseta = numeroCamiseta;
 	}
+
+	public int getCantasistencias() {
+		return cantasistencias;
+	}
+
+	public void setCantasistencias(int cantasistencias) {
+		this.cantasistencias = cantasistencias;
+	}
+
+	public int getCantbloqueo() {
+		return cantbloqueo;
+	}
+
+	public void setCantbloqueo(int cantbloqueo) {
+		this.cantbloqueo = cantbloqueo;
+	}
+
+	public int getCantrobodebalon() {
+		return cantrobodebalon;
+	}
+
+	public void setCantrobodebalon(int cantrobodebalon) {
+		this.cantrobodebalon = cantrobodebalon;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	
+
 }
